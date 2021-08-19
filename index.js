649fa0ccd9670b5fbb4477396d2faff2307ff7fd
gurisaka <guri95saka22@gmail.com>
2021-08-20 04:19:32 +0900

    armsからweaponsに改名&weapons基本機能実装

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ const app = Vue.createApp({
 			abilities:[],
 			skills:[],
 			combats:[],
+			weapons:[],
 			show_add_cards: true
 		}
 	},
 	methods:{
 		save_sheet(){
-			var blob = new Blob([JSON.stringify(this.skills)], {type: "text/plain;charset=utf-8"});
+			let json_data = {};
+			json_data["identities"] = this.identities;
+			json_data["statuses"] = this.statuses;
+			json_data["abilities"] = this.abilities;
+			json_data["skills"] = this.skills;
+			json_data["combats"] = this.combats;
+			json_data["weapons"] = this.weapons;
+
+			var blob = new Blob(
+				[JSON.stringify(json_data)], {type: "text/plain;charset=utf-8"});
 			saveAs(blob, "sheet.json");
 		},
 		load_sheet(event){
@@ -28,6 +38,7 @@ const app = Vue.createApp({
 				this.abilities = json["abilities"];
 				this.skills = json["skills"];
 				this.combats = json["combats"];
+				this.weapons = json["weapons"];
 			};
 		},
 		delete_card(event_values){
@@ -50,6 +61,8 @@ const app = Vue.createApp({
 				case 'combat':
 					cards = this.combats;
 					break;
+				case 'weapon':
+					cards = this.weapons;
 			}
 
 			cards.forEach((card, index) => {
@@ -104,6 +117,16 @@ const app = Vue.createApp({
 
 					};
 					break;
+				case 'weapon':
+					cards = this.weapons;
+					change_method = function(cards, index, value){
+						if(value[0] != "damage" && value[0] != "status"){
+							cards[index][value[0]] = parseInt(value[1]);
+						}else{
+							cards[index][value[0]] = value[1];
+						}
+					}
+					break;
 			}
 
 			cards.forEach((card, index_) =>{
@@ -111,7 +134,6 @@ const app = Vue.createApp({
 					index = index_;
 				}
 			});
-
 			change_method(cards, index, value);
 		},
 		add_new_card(event){
@@ -130,7 +152,7 @@ const app = Vue.createApp({
 						return !!name;
 					};
 					card_push_method = function(cards, name, value){
-						cards.push({name: new_card_name, value: new_card_value});
+						cards.push({name: name, value: value});
 					};
 					break;
 
@@ -144,7 +166,7 @@ const app = Vue.createApp({
 						return true;
 					};
 					card_push_method = function(cards, name, value){
-						cards.push({name: new_card_name, value: parseInt(new_card_value)});
+						cards.push({name: name, value: parseInt(value)});
 					};
 					break;
 
@@ -158,7 +180,7 @@ const app = Vue.createApp({
 						return true;
 					};
 					card_push_method = function(cards, name, value){
-						cards.push({name: new_card_name, value: parseInt(new_card_value)});
+						cards.push({name: name, value: parseInt(value)});
 					};
 					break;
 
@@ -171,18 +193,44 @@ const app = Vue.createApp({
 						return true;
 					};
 					card_push_method = function(cards, name, value){
-						cards.push({name: new_card_name, value: parseInt(new_card_value), is_checked: false});
+						cards.push({name: name, value: parseInt(value), is_checked: false});
+					};
+					break;
+				case 'weapon':
+					cards = this.weapons;
+					value_check_method = function(name, value){
+						if(name == null || !name)return false;
+						return true;
+					};
+					card_push_method = function(cards, name, values){
+						cards.push({name: name, regular: parseInt(values[0]), hard: parseInt(values[1]), extream: parseInt(values[2]),
+									damage: values[3], range: parseInt(values[4]), attack_times: parseInt(values[5]), bullets: parseInt(values[6]),
+									failure: parseInt(values[7]), status:values[8]});
 					};
 					break;
 			}
 
-			new_card_name  = document.getElementsByClassName("new-" + new_card_type + "-name")[0].value;
-			new_card_value = document.getElementsByClassName("new-" + new_card_type + "-value")[0].value;
-			document.getElementsByClassName("new-" + new_card_type + "-name")[0].value = null;
-			if(new_card_type == "identity"){
-				document.getElementsByClassName("new-" + new_card_type + "-value")[0].value = null;
+			if(new_card_type != "weapon"){
+				new_card_name  = document.getElementsByClassName("new-" + new_card_type + "-name")[0].value;
+				new_card_value = document.getElementsByClassName("new-" + new_card_type + "-value")[0].value;
+				document.getElementsByClassName("new-" + new_card_type + "-name")[0].value = null;
+				if(new_card_type == "identity"){
+					document.getElementsByClassName("new-" + new_card_type + "-value")[0].value = null;
+				}else{
+					document.getElementsByClassName("new-" + new_card_type + "-value")[0].value = 0;
+				}
 			}else{
-				document.getElementsByClassName("new-" + new_card_type + "-value")[0].value = 0;
+				new_card_name = document.getElementsByClassName("new-" + new_card_type + "-name")[0].value;
+				new_card_value = [];
+				new_card_value.push(document.getElementsByClassName("new-" + new_card_type + "-regular")[0].value);
+				new_card_value.push(document.getElementsByClassName("new-" + new_card_type + "-hard")[0].value);
+				new_card_value.push(document.getElementsByClassName("new-" + new_card_type + "-extream")[0].value);
+				new_card_value.push(document.getElementsByClassName("new-" + new_card_type + "-damage")[0].value);
+				new_card_value.push(document.getElementsByClassName("new-" + new_card_type + "-range")[0].value);
+				new_card_value.push(document.getElementsByClassName("new-" + new_card_type + "-attack_times")[0].value);
+				new_card_value.push(document.getElementsByClassName("new-" + new_card_type + "-bullets")[0].value);
+				new_card_value.push(document.getElementsByClassName("new-" + new_card_type + "-failure")[0].value);
+				new_card_value.push(document.getElementsByClassName("new-" + new_card_type + "-status")[0].value);
 			}
 
 			if(!value_check_method(new_card_name, new_card_value))return;
@@ -372,6 +420,79 @@ app.component("combat_card",{
 		},
 		deleteCard(event){
 			this.$emit('delete-card', [event, this.name, "combat"]);
+		}
+	}
+})
+
+app.component("weapon_card",{
+	props: ['name', 'regular', 'hard', 'extream', 'damage', 'range', 'attack_times', 'bullets', 'failure', 'status'],
+	props: {
+		name: {
+			type: String,
+			required: true
+		},
+		regular: {
+			type: Number,
+			required: false
+		},
+		hard: {
+			type: Number,
+			required: false
+		},
+		extream: {
+			type: Number,
+			required: false
+		},
+		damage: {
+			type: String,
+			required: false
+		},
+		range: {
+			type: Number,
+			required: false
+		},
+		attack_times: {
+			type: Number,
+			required: false
+		},
+		bullets: {
+			type: Number,
+			required: false
+		},
+		failure: {
+			type: Number,
+			required: false
+		},
+		status: {
+			type: String,
+			required: false
+		}
+	},
+	template: `
+		<div style="display: flex;align-items: center; padding: 0.5em; padding-top:0.1em; padding-bottom: 0.1em; border-bottom: 1px black solid ; width: 35.5em;">
+			<div style="margin-left: 1.5em; width: 4.5em; min-width: 4.5em; font-size:medium" @click.right.ctrl="deleteCard">
+				{{name}}:
+			</div>
+			<div style="margin-left: 0.4em; display: flex">
+				<input :value="regular" id="weapon-regular" type="number" style="margin-left: 0.3em;width: 2.5em" @change="updateCardStatus">
+				<input :value="hard" id="weapon-hard" type="number" style="margin-left: 0.3em;width: 2.5em" @change="updateCardStatus">
+				<input :value="extream" id="weapon-extream" type="number" style="margin-left: 0.3em;width: 2.5em" @change="updateCardStatus">
+				<input :value="damage" id="weapon-damage" type="text" style="margin-left: 0.3em;width: 5em" @change="updateCardStatus">
+				<input :value="range" id="weapon-range" type="number" style="margin-left: 0.3em;width: 2.5em" @change="updateCardStatus">
+				<input :value="attack_times" id="weapon-attack_times" type="number" style="margin-left: 0.3em;width: 2.5em" @change="updateCardStatus">
+				<input :value="bullets" id="weapon-bullets" type="number" style="margin-left: 0.3em;width: 2.5em" @change="updateCardStatus">
+				<input :value="failure" id="weapon-failure" type="number" style="margin-left: 0.3em;width: 2.5em" @change="updateCardStatus">
+				<input :value="status" id="weapon-status" type="text" style="margin-left: 0.3em;width: 3em" @change="updateCardStatus">
+			</div>
+		</div>			
+	`,
+
+	methods: {
+		updateCardStatus(event){
+			this.$emit('update-card-status', {card_type: "weapon", card_name: this.name, value: [event.target.id.split("-")[1], event.target.value]});
+		},
+		deleteCard(event){
+			this.$emit('delete-card', [event, this.name, "weapon"]);
 		}
 	}
 })
